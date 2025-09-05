c983f81f71042a93f8502515db8881384ec1e1e6
Jonathan <aguilarjonathan2902@gmail.com>
2025-09-05 10:57:38 -0300

    Agrego el ejercicio 8 sin pruebas

--- TrabajoPractico3/Hospital.java
+++ TrabajoPractico3/Hospital.java
@@ -11,9 +11,30 @@ public class Hospital
     private String nombreDirector;
     
     public Hospital(String p_hospital, String p_director){
-        setNombreHospital(p_hospital);
-        setNombreDirector(p_director);
+        this.setNombreHospital(p_hospital);
+        this.setNombreDirector(p_director);
     }
     
+    private void setNombreHospital(String p_hospital){
+        this.nombreHospital = p_hospital;
+    }
+    
+    private void setNombreDirector(String p_director){
+        this.nombreDirector = p_director;
+    }
+    
+    public String getNombreHospital(){
+        return this.nombreHospital;
+    }
+    
+    public String getNombreDirector(){
+        return this.nombreDirector;
+    }
+    
+    public void consultaDatosFiliatorios(Paciente p_paciente){
+        System.out.println("\tHospital: " + this.getNombreHospital() + " Director: " + this.getNombreDirector());
+        System.out.println("\t--------------------------------------------------------------------------------\n");
+        p_paciente.mostrarDatosPantalla();
+    }
     
 }
--- TrabajoPractico3/Paciente.java
+++ TrabajoPractico3/Paciente.java
@@ -1,4 +1,3 @@
-
 /**
  * Write a description of class Pacient here.
  * 
@@ -7,4 +6,67 @@
  */
 public class Paciente
 {
+    private int historiaClinica;
+    private String nombre;
+    private String domicilio;
+    private Localidad localidadNacido;
+    private Localidad localidadVive;
+    
+    public Paciente(int p_historia, String p_nombre, String p_domicilio, Localidad p_localidadNacido, Localidad p_localidadVive){
+        this.setHistoriaClinica(p_historia);
+        this.setNombre(p_nombre);
+        this.setDomicilio(p_domicilio);
+        this.setNacido(p_localidadNacido);
+        this.setVive(p_localidadVive);
+    }
+    
+    private void setHistoriaClinica(int p_historia){
+        this.historiaClinica = p_historia;
+    }
+    
+    private void setNombre(String p_nombre){
+        this.nombre = p_nombre;
+    }
+    
+    private void setDomicilio(String p_domicilio){
+        this.domicilio = p_domicilio;
+    }
+    
+    private void setNacido(Localidad p_nacido){
+        this.localidadNacido = p_nacido;
+    }
+    
+    private void setVive(Localidad p_vive){
+        this.localidadVive = p_vive;
+    }
+    
+    public int getHistoriaClinica(){
+        return this.historiaClinica;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public String getDomicilio(){
+        return this.domicilio;
+    }
+    
+    public Localidad getNacido(){
+        return this.localidadNacido;
+    }
+    
+    public Localidad getVive(){
+        return this.localidadVive;
+    }
+    
+    public void mostrarDatosPantalla(){
+        System.out.println("\tPaciente: " + this.getNombre() + " Historia Clinica: " + this.getHistoriaClinica() + " Domicilio: " + this.getDomicilio());
+        this.getVive().mostrar();
+    }
+    
+    public String cadenaDeDatos(){
+        return this.getNombre() + " ...... " + this.getHistoriaClinica() + " ..... " + this.getDomicilio() + " - " + this.getVive().getNombre() + " - " + this.getVive().getProvincia();
+    }
+    
 }
